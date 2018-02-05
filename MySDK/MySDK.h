b864ac83ca2791646434ec52c1906739ace640bb
kevser.unalan <kevser.temiz@gmail.com>
2018-02-05 14:55:33 +0300

    initial commit

---
+++ MySDK/MySDK.h
@@ -0,0 +1,19 @@
+//
+//  MySDK.h
+//  MySDK
+//
+//  Created by Xevser on 5.02.2018.
+//  Copyright © 2018 Medyasoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MySDK.
+FOUNDATION_EXPORT double MySDKVersionNumber;
+
+//! Project version string for MySDK.
+FOUNDATION_EXPORT const unsigned char MySDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MySDK/PublicHeader.h>
+
+
---
+++ MySDK/MySDK.swift
@@ -0,0 +1,16 @@
+//
+//  MySDK.swift
+//  MySDK
+//
+//  Created by Xevser on 5.02.2018.
+//  Copyright © 2018 Medyasoft. All rights reserved.
+//
+
+import Foundation
+
+public class MySDK {
+    public class func logToConsole(msg: String) {
+        print(msg);
+    }
+}
+
